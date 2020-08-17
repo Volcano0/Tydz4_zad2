01d144e4a9ebe8ef7dc647cad6840b42af42fab1
Gabriel Cwik <gabriel.cwik@cgi.com>
2020-08-17 15:54:48 +0200

    Adding project files again

---
+++ Tydz4_zad2/FizzBuzz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tydz4_zad2
+{
+    class FizzBuzz
+    {
+        public string FizzBuzzCheck(int number)
+        {
+
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+            else if (number % 3 == 0)
+                return "Fizz";
+            else if (number % 5 == 0)
+                return "Buzz";
+            else
+                return $"Podana liczba to: {number}";
+            
+        }
+    }
+}
---
+++ Tydz4_zad2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tydz4_zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Podaj liczbę");
+                var number = int.Parse(Console.ReadLine());
+                var FizzBuzz = new FizzBuzz();
+
+                var result = FizzBuzz.FizzBuzzCheck(number);
+
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Nie podano liczby");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
